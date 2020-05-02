ec099e148c43c3f21a56f086125cbf33c424c84a
Abigail Limpioso <alimpioso@gmail.com>
2020-05-02 12:17:03 -0400

    add documentation to script function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-
+//Updates user icon on top right on load
 function updateUserIcon(){
 
     var savedIconIdentifier = localStorage.getItem("userIcon");
 
     if(savedIconIdentifier) {
-
+        //USE SAVED IDENTIFIER TO LOAD THE SAME USER AVATAR
         var svg = $("<img>");
         svg.addClass("avatar-img");
         svg.attr("src", "https://avatars.dicebear.com/v2/avataaars/avatar"+savedIconIdentifier+".svg?mood=happy&radius=30&width=75&background=%23FFFFFF");
